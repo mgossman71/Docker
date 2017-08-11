8e14bcb485576f9b35d9785f221a6772a8abaf72
gozz <gozz@gozzs-MacBook-Air.local>
2017-08-11 10:58:00 -0500

    updates

---
+++ Redeploy-Plex.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+for i in `docker ps -a |grep -i plex |awk '{print $1}'`
+do
+	docker stop $i
+    docker rm $i
+done
+
+for i in `docker images |grep -i plex |awk '{print $3}'`
+do
+	docker rmi $i
+done
+
+docker run -dt --name plex --restart=always \
+--net=host \
+-v /docker/plex:/config \
+-v /mnt:/mnt \
+-e TZ='America/Chicago' \
+-e PGID=0 -e PUID=0 \
+-p 32400:32400 \
+linuxserver/plex
---
+++ Redeploy-sabnzb.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+for i in `docker ps -a |grep -i sabnzb |awk '{print $1}'`
+do
+	docker stop $i
+    docker rm $i
+done
+
+for i in `docker images |grep -i sabnzb |awk '{print $3}'`
+do
+	docker rmi $i
+done
+
+docker run -dt --name sabnzb --restart=always \
+--net=host \
+-v /docker/sabnzb:/config \
+-v /mnt:/mnt \
+-e TZ='America/Chicago' \
+-e PGID=0 -e PUID=0 \
+-p 8080:8080 \
+linuxserver/sabnzbd
+
