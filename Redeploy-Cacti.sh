62a6eab521f340b7a51dd87292c2896c04a336f5
gozz <gozz@gozzs-air.gozz.domain>
2017-08-12 18:56:28 -0500

    update

---
+++ Redeploy-Cacti.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+for i in `docker ps -a |grep -i cacti |awk '{print $1}'`
+do
+	docker stop $i
+    docker rm $i
+done
+
+for i in `docker images |grep -i cacti |awk '{print $3}'`
+do
+	docker rmi $i
+done
+
+# --net=host \
+docker run \
+-dt \
+-p 80  \
+quantumobject/docker-cacti
